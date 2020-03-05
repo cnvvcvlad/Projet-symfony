e91d85b395449f10017910dc352686e24b65ac7b
cnvvcvlad <cnvvc_vlad@yahoo.fr>
2020-03-05 18:42:07 +0100

    add english and french languages

--- src/Controller/FrontController.php
+++ src/Controller/FrontController.php
@@ -3,7 +3,9 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
+
 
 class FrontController extends AbstractController
 {
@@ -16,6 +18,19 @@ class FrontController extends AbstractController
             'variable' => 'C\'est une variable cliquable'
         ]);
     }
+
+    /**
+     * @Route("/change_locale/{locale}", name="change_locale")
+     */
+    public function changeLocale($locale, Request $request)
+    {
+        // On stocke la langue dans la session
+        $request->getSession()->set('_locale', $locale);
+
+        // On revient sur la page précédente
+        return $this->redirect($request->headers->get('referer'));
+    }
+
 
     /**
      * @return mixed
