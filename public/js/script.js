041161eb0de7dbd1c50d09751bfbaf0105bf2924
cnvvcvlad <cnvvc_vlad@yahoo.fr>
2020-05-12 10:33:52 +0200

    Add a simple burger menu

---
+++ public/js/script.js
@@ -0,0 +1,8 @@
+/**
+ * Created by user on 12.05.2020.
+ */
+function onClickMenu(){
+    document.getElementById("menu").classList.toggle("change");
+    document.getElementById("navigate").classList.toggle("change");
+    document.getElementById("menu-bg").classList.toggle("change-bg");
+}
