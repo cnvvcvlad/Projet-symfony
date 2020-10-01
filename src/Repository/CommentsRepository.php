ae28afc01742107034e91b55cf08bc089be6a16d
cnvvcvlad <cnvvc_vlad@yahoo.fr>
2020-10-01 13:47:10 +0200

    changed the path repository

--- src/Repository/CommentsRepository.php
+++ src/Repository/CommentsRepository.php
@@ -4,7 +4,7 @@ namespace App\Repository;
 
 use App\Entity\Comments;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
-use Doctrine\Common\Persistence\ManagerRegistry;
+use Doctrine\Persistence\ManagerRegistry;
 
 /**
  * @method Comments|null find($id, $lockMode = null, $lockVersion = null)
--- src/Repository/MotsClesRepository.php
+++ src/Repository/MotsClesRepository.php
@@ -4,7 +4,7 @@ namespace App\Repository;
 
 use App\Entity\MotsCles;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
-use Doctrine\Common\Persistence\ManagerRegistry;
+use Doctrine\Persistence\ManagerRegistry;
 
 /**
  * @method MotsCles|null find($id, $lockMode = null, $lockVersion = null)
