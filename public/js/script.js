578a7ff6a96dee2361abe2c2956f546e50930c30
cnvvcvlad <cnvvc_vlad@yahoo.fr>
2021-02-10 16:24:36 +0100

    add files scss

--- public/js/script.js
+++ public/js/script.js
@@ -1,8 +1,10 @@
 /**
  * Created by user on 12.05.2020.
  */
-function onClickMenu(){
+function onClickMenu() {
     document.getElementById("menu").classList.toggle("change");
     document.getElementById("navigate").classList.toggle("change");
     document.getElementById("menu-bg").classList.toggle("change-bg");
 }
+
+console.log('salut')
